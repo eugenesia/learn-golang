fec43dbcedf97c67edfa662a5b0c7207cfb57fc8
Eugene Sia <eugene@eugenesia.co.uk>
2025-05-16 01:43:48 +0100

    Return an error

--- create-module/greetings/greetings.go
+++ create-module/greetings/greetings.go
@@ -1,13 +1,21 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
 // In Go, a function whose name starts with a capital letter is exported and
 // can be called from other packages.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// Return a greeting that embeds the name in a message.
 	// := declares and initialises a variable in one step.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- create-module/hello/hello.go
+++ create-module/hello/hello.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
+	// Set propertiews of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		// log.Fatal(err) is equivalent to log.Print(err) followed by os.Exit(1).
+		// It prints the error message and exits the program with a non-zero status.
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message.
 	fmt.Println(message)
 }
